b48660798fa9e86644e953c6f8b25a85a62cdb29
AuctionStakeProfit <74728072+AuctionStakeProfit@users.noreply.github.com>
2020-12-22 09:03:15 -0800

    Update contracts.js

--- js/contracts.js
+++ js/contracts.js
@@ -109,10 +109,11 @@ function sortTable(tableId) {
 
 			x = rows[i].getElementsByClassName("value")
 			console.log(x)
+			console.log(x.innerHTML)
+			console.log(parseInt(x.innerHTML))
 
 			y = rows[i + 1].getElementsByClassName("value")
-			console.log(y)
-
+			
 			if (parseInt(x.innerHTML) < parseInt(y.innerHTML)) {
 			//if so, mark as a switch and break the loop:
 			shouldSwitch = true
