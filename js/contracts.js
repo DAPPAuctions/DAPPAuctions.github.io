49028121030ba23ed6ce50f74bc3f3bbfbc3fbee
AuctionStakeProfit <74728072+AuctionStakeProfit@users.noreply.github.com>
2020-12-23 07:05:22 -0800

    Update contracts.js

--- js/contracts.js
+++ js/contracts.js
@@ -21,8 +21,6 @@ trxContracts[8] = {name: "moons", addr: "TCm3MnZcz5ZTRWMVTYX6P32XMGLXrdZuo7"}
 window.addEventListener('load', function () {
     web3 = new Web3(new Web3.providers.HttpProvider("https://mainnet.infura.io/v3/46fa67a3222a4d1fb00e93bd7b6c7bbb"));
 	
-	import TronWeb from 'web3.min.js';
-
 	const HttpProvider = TronWeb.providers.HttpProvider;
 	const fullNode = new HttpProvider('https://api.trongrid.io');
 	const solidityNode = new HttpProvider('https://api.trongrid.io');
