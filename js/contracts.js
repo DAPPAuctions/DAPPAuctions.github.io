c2f0c23a72747d891a90bb027e9617b389f51f1f
AuctionStakeProfit <74728072+AuctionStakeProfit@users.noreply.github.com>
2020-12-22 21:50:06 -0800

    Update contracts.js

--- js/contracts.js
+++ js/contracts.js
@@ -111,7 +111,7 @@ function sortTable(tableId) {
 			x2 = rows[i].getElementsByClassName("#")
 			y2 = rows[i + 1].getElementsByClassName("#")
 			
-			console.log(parseInt(x[0].innerHTML))
+			console.log(x[0].innerHTML)
 			if (parseInt(x[0].innerHTML) < parseInt(y[0].innerHTML)) {
 				shouldSwitch = true
 				break
