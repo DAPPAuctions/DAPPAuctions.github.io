01599062f51fe660026ae0dc5a7dd2721d7902b0
DAPPAuctions <74728072+DAPPAuctions@users.noreply.github.com>
2020-12-30 21:15:28 -0800

    Update contracts.js

--- js/contracts.js
+++ js/contracts.js
@@ -287,7 +287,7 @@ function sortTable(tableId) {
 			y = rows[i + 1].getElementsByClassName("value")
 			x2 = rows[i].getElementsByClassName("#")
 			y2 = rows[i + 1].getElementsByClassName("#")
-			if ( x[0].innerHTML.replace(/,/g, '') < y[0].innerHTML.replace(/,/g, '') ) {
+			if ( x[0].innerHTML < y[0].innerHTML ) {
 				shouldSwitch = true
 				break
 			}
